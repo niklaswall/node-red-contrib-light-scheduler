80b15da8462b066c7d14b84090dc7ac88e8d3d46
Niklas Wall <niklas@wall.am>
2021-03-09 08:54:11 +0100

    Added GeoIP and Browser location info in settings.

--- light-scheduler-settings.js
+++ light-scheduler-settings.js
@@ -1,9 +1,32 @@
-module.exports = function(RED) {
+const axios = require('axios')
+var pjson = require('./package.json')
+
+module.exports = function (RED) {
   function LightSchedulerSettings(n) {
-    RED.nodes.createNode(this, n);
-    this.name = n.name;
-    this.latitude = n.latitude;
-    this.longitude = n.longitude;
+    RED.nodes.createNode(this, n)
+    this.name = n.name
+    this.latitude = n.latitude
+    this.longitude = n.longitude
   }
-  RED.nodes.registerType("light-scheduler-settings", LightSchedulerSettings);
+  RED.nodes.registerType('light-scheduler-settings', LightSchedulerSettings)
+
+  // Get latitude/longitude using geoip lookup.
+  RED.httpAdmin.get('/light-scheduler/geoinfo.json', async function (req, res) {
+    try {
+      const response = await axios.get(
+        'https://2quhxokvva.execute-api.eu-west-1.amazonaws.com/default/light-scheduler-geoip?v=' + pjson.version
+      )
+      if (response.status != 200) {
+        res.status(500)
+        res.json(null)
+      }
+      let data = response.data
+      res.status(200)
+      res.json({city: data.city, country: data.country_name, latitude: data.latitude, longitude: data.longitude})
+    } catch (err) {
+      console.error(err)
+      res.status(500)
+      res.json(null)
+    }
+  })
 }
